bd1d371291c78bc139d6ddd3fc2f01285b7b6c5f
Dylan Meeus <meeusdylan@hotmail.com>
2017-12-10 00:05:14 +0100

    Solve day 1, part 1

---
+++ 2017/9.py
@@ -0,0 +1,57 @@
+# solution to day 9 of advent of code
+
+
+
+
+
+# Each time you close, {, the score increments with the 'depth'
+def parse(data):
+    score = 0
+    open_groups = 0
+    parsing_garbage = False
+    for character in data:
+        # guards, guards!
+        if parsing_garbage and not character == ">":
+            continue
+
+        if character == "<":
+            parsing_garbage = True
+        elif character == ">":
+            parsing_garbage = False
+        elif character == "{":
+            open_groups += 1
+        elif character == "}":
+            score += open_groups
+            open_groups -= 1
+    return score
+   
+
+
+
+def sanitize(data):
+    """ Apply escape character to text """
+    out = ""
+    i = 0
+    while i < (len(data)):
+        char = data[i]
+        if char == "!":
+            i += 2
+            continue
+        out += char
+        i += 1
+    return out
+
+
+def getInput():
+    f = open("input9.txt","r")
+    return f.read()
+
+
+
+
+def solve():
+    data = getInput()
+    score = parse(sanitize(data))
+    print(score)
+
+solve()
