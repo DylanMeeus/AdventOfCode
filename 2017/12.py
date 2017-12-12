70b6d723d7cf467b717f8be34efb487b368aba57
Dylan Meeus <meeusdylan@hotmail.com>
2017-12-12 22:15:53 +0100

    Day 12, part 2

--- 2017/12.py
+++ 2017/12.py
@@ -1,12 +1,54 @@
 # Solution to Day 12
 
 
+class group:
+    def __init__(self, g):
+        self.group = g
+
+    def contains(self, x):
+        return x in self.group
+
+    def add(self, xs):
+        self.group.extend(xs)
 
 def getInput():
     f = open('input12.txt','r')
     data = f.read()
     return data.split("\n")[:-1]
 
+
+def solve2():
+    data = getInput()
+    heads = []
+    
+    for d in data:
+        head = d.split(' ')[0]
+        heads.append(head)
+        heads = list(map(lambda k: int(k),heads))
+    
+    groups = []
+    while len(heads) > 0:
+        # Create a chain
+        chain = create_link(heads[0],[],data)
+        print(chain)
+        if chain == None:
+            continue
+        # filter unique elements
+        chain = map(lambda k: int(k),chain)
+        unique = set(chain)
+        for existing_group in groups:
+            for el in unique:
+                if existing_group.contains(el):
+                    existing_group.add(unique)
+                    print("going to break")
+                    break
+        else:
+            gr = group(unique)
+            groups.append(gr)
+        print(len(heads))
+        heads = list(filter(lambda k: k not in unique,heads))
+        print(len(heads))
+    print(len(groups))
 
 def solve():
     data = getInput()
@@ -39,4 +81,7 @@ def create_link(node, chain,data):
             create_link(int(tail),chain,data)
 
     return chain
-solve()
+
+
+#solve()
+solve2()
