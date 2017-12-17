b4c2f7112ea20f2b2d0e5f098275ad0564693c53
Dylan Meeus <meeusdylan@hotmail.com>
2017-12-17 04:20:02 +0100

    Day 15, part2

--- 2017/15.py
+++ 2017/15.py
@@ -3,9 +3,35 @@
 
 
 
+def solve2():
+    A = 277
+    B = 349
+    A_mult = 16807
+    B_mult = 48271
+    divisor = 2147483647
+
+    mask = 0xffff
+    As = []
+    Bs = []
+    limit = int(5*(10**6))
+    while len(As) < limit or len(Bs) < limit:
+        if len(As) < limit:
+            A = next_val(A,A_mult,divisor)
+            if A % 4 == 0:
+                As.append(A)
+        if len(Bs) < limit: 
+            B = next_val(B,B_mult,divisor)
+            if B % 8 == 0:
+                Bs.append(B)
+    print(find_matches(As,Bs,mask))
 
 
-
+def find_matches(Xs,Ys,mask):
+    matches = 0
+    for i in range(len(Xs)):
+        if Xs[i] & mask == Ys[i] & mask:
+            matches += 1
+    return matches
 
 
 
@@ -35,4 +61,5 @@ def next_val(init, multiplier, divisor):
 
 
 
-solve()
+#solve()
+solve2()
