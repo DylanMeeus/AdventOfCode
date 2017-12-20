79551343b0d64530152b53a5c096c0504542098e
Dylan Meeus <meeusdylan@hotmail.com>
2017-12-20 22:19:49 +0100

    Day 17, part 2

--- 2017/17.py
+++ 2017/17.py
@@ -2,6 +2,25 @@
 from collections import deque
 
 
+
+def solve2():
+    print("hello world")
+    steps = 369 
+
+    value = 1
+    location = 0
+    inserts = 1 
+    second_position = 0
+    for i in range(50000000):
+        location = (location + steps) % inserts
+        location += 1 # step one more to the right
+        if location == 1:
+            second_position = value
+        value += 1
+        inserts += 1
+    print(second_position)
+    
+
 def solve():
     print("hello world")
     steps = 369 
@@ -21,4 +40,5 @@ def solve():
             print(que[i+1])
             exit()
   
-solve()
+#solve()
+solve2()
