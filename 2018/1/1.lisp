39e7c6aafe668cc14289db45b1a9d07e892dc168
dylan <meeusdylan@protonmail.com>
2018-12-01 21:03:07 +0100

    solve part 2

--- 2018/1/1.lisp
+++ 2018/1/1.lisp
@@ -6,7 +6,22 @@
           while line
           collect line)))
 
+(defun get-test()
+  '("1" "-2" "3" "1"))
 
 (defun solve() 
   (let ((input (mapcar (lambda (s) (parse-integer s)) (get-file))))
     (apply '+ input)))
+
+
+(defun solve2() 
+  (let ((input (mapcar (lambda (s) (parse-integer s)) (get-file))))
+    (next (car input) (cdr input) '(0))))
+
+(defun next(f fs pfs)
+  (let ((next_value (+ f (car fs))))
+    (cond ((eq next_value (find next_value pfs))
+           (write next_value))
+          ((not (eq nil (cdr fs)))
+            (next next_value (cdr fs) (cons next_value pfs)))
+          (t (next next_value (mapcar (lambda (s) (parse-integer s)) (get-file)) (cons next_value pfs))))))
