63082508ace0226ba9ef5ea58830902edd8f7970
dylan <meeusdylan@protonmail.com>
2018-12-02 18:21:41 +0100

    solve 2

--- 2018/2/2.go
+++ 2018/2/2.go
@@ -9,6 +9,37 @@ import (
 
 func main() {
     fmt.Printf("%v\n", solve())
+    fmt.Printf("%v\n", solve2())
+}
+
+
+func solve2() string {
+    codes := codes()
+    for _,code := range codes {
+        for _,other := range codes {
+            diff, common := diffs(code, other)
+            if diff == 1 {
+                return common
+            }
+        }
+    }
+    return ""
+}
+
+func diffs(one, two string) (diff int, common string) {
+    xs := strings.Split(one, "")
+    ys := strings.Split(two, "")
+    if len(xs) != len(ys) {
+        return 0, ""
+    }
+    for i,x := range xs {
+        if x != ys[i] {
+            diff++
+        } else {
+            common += x
+        }
+    }
+    return diff, common
 }
 
 func solve() int {
