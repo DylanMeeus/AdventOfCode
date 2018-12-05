378f8f70335c5e5c8f4f455bcb83706853073ed0
dylan <meeusdylan@protonmail.com>
2018-12-05 10:45:57 +0100

    solve day 5 part 1

---
+++ 2018/5/5.py
@@ -0,0 +1,19 @@
+""" Solution to Advent Of Code 5 """
+from functools import reduce
+
+def test(f, polymer):
+    return f(polymer) == "dabCBAcaDA"
+
+
+def react(polymer):
+    if polymer == "":
+        return ""
+    res = reduce(lambda x, y:  x[:-1] if x[-1:] != y and x.lower()[-1:] == y.lower() else x + y, polymer)
+    if len(res) == len(polymer):
+        return res
+    return react(res)
+
+
+if __name__ == '__main__':
+    polymer = open('input.txt','r').read().replace("\n","")
+    print(len(react(polymer)))
