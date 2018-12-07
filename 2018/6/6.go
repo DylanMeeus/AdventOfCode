f684e93891a51ea863f141c4941e5e2c7842e49b
dylan <meeusdylan@protonmail.com>
2018-12-07 02:54:02 +0100

    refactor to use bounds struct

--- 2018/6/6.go
+++ 2018/6/6.go
@@ -12,19 +12,24 @@ type point struct{
     id, x, y int
 }
 
+type boundary struct {
+    left,right,top,bottom int
+}
+
 
 func main() {
     ps := points()
-    l,t,r,b := boundingBox(ps)
-    //f := field(l,t,r,b)
-    //fmt.Printf("%v\n", solve(f,ps,l,t,r,b))
-    fmt.Printf("%v\n", solve2(ps, l,t,r,b))
+    bounds := boundingBox(ps)
+    f := field(bounds)
+    fmt.Printf("%v\n", solve(f,ps, bounds))
+    fmt.Printf("%v\n", solve2(ps, bounds))
 }
 
-func solve2(positions []*point, left, top, right, bottom int) int {
+func solve2(positions []*point, bounds *boundary) int {
     // we grow outside of our bounding box, because we know the max distance from it is 10.000
     // we can use maxdistance (h) from top instead of bottom, due to connecting to all points
     // and actually we can use the diff from top-bottom, but this is bruteforce anyway :)
+    left, right, top, bottom := bounds.left, bounds.right, bounds.top, bounds.bottom
     maxdistance := 10000 
     cpos := len(positions)
     left -= (maxdistance / cpos)
@@ -50,7 +55,7 @@ func solve2(positions []*point, left, top, right, bottom int) int {
 }
 
 
-func solve(field []*point, positions []*point, left, top , right, bottom int) (max int) {
+func solve(field []*point, positions []*point, bounds *boundary) (max int) {
     // foreach point in the field, find the closest coordinate
     for _,point := range field {
         dist := 1000
@@ -68,7 +73,7 @@ func solve(field []*point, positions []*point, left, top , right, bottom int) (m
     count := make(map[int]int, 0)
     for _,p := range field {
         // make sure the p is valid
-        if valid(p, field, left,top,right,bottom) {
+        if valid(p, field, bounds) {
             count[p.id]++
         }
     }
@@ -84,10 +89,10 @@ func solve(field []*point, positions []*point, left, top , right, bottom int) (m
     return max
 }
 
-func valid(p *point, field []*point, left, top, right, bottom int) bool {
+func valid(p *point, field []*point, bounds *boundary) bool {
     for _, fp := range field {
         if p.id == fp.id {
-            if fp.y == left || fp.y == right || fp.x == top || fp.x == bottom {
+            if fp.y == bounds.left || fp.y == bounds.right || fp.x == bounds.top || fp.x == bounds.bottom {
                 return false
             }
         }
@@ -100,10 +105,10 @@ func distance(p1, p2 *point) int {
     return int(math.Abs(float64(p2.x - p1.x)) + math.Abs(float64(p2.y-p1.y)))
 }
 
-func field(left, top, right, bottom int) []*point {
+func field(bounds *boundary) []*point {
     f := make([]*point, 0)
-    for row := top; row <= bottom; row++ {
-        for col := left; col <= right; col++ {
+    for row := bounds.top; row <= bounds.bottom; row++ {
+        for col := bounds.left; col <= bounds.right; col++ {
             f = append(f, &point{-1, row, col})
         }
     }
@@ -111,23 +116,24 @@ func field(left, top, right, bottom int) []*point {
 }
 
 
-func boundingBox(points []*point) (left,top,right,bottom int) {
-    left, top = 1000, 1000
+func boundingBox(points []*point) *boundary {
+    bounds := &boundary{}
+    bounds.left, bounds.top = 1000, 1000
     for _,p := range (points) {
-        if p.x > bottom {
-            bottom = p.x
+        if p.x > bounds.bottom {
+            bounds.bottom = p.x
         }
-        if p.x < top {
-            top = p.x
+        if p.x < bounds.top {
+            bounds.top = p.x
         }
-        if p.y > right {
-            right = p.y
+        if p.y > bounds.right {
+            bounds.right = p.y
         }
-        if p.y < left {
-            left = p.y
+        if p.y < bounds.left {
+            bounds.left = p.y
         }
     }
-    return
+    return bounds
 }
 
 func points() []*point{
