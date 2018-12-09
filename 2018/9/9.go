48f3ff4f15fc55e6dea4a8f7dce86f0c7b702b7f
dylan <meeusdylan@protonmail.com>
2018-12-09 21:41:20 +0100

    solve part 1

--- 2018/9/9.go
+++ 2018/9/9.go
@@ -16,11 +16,26 @@ func solve(){
     // always place a marble between current / current + 1
 
     l.Add(0)
-    for marble := 1; marble < 8; marble++{ 
-        fmt.Printf("%v\n", l.String())
-        fmt.Println(marble)
-        fmt.Println(currentIndex)
-        fmt.Println(l.Size)
+    players := 470 
+    pScores := make(map[int]int, 0)
+    currentPlayer := 0
+    for marble := 1; marble <= 72170; marble++{ 
+        if marble % 23 == 0 {
+            pScores[currentPlayer] += marble 
+            previous := currentIndex - 7 - 1
+            if previous < 0 {
+                diff := 0 - previous
+                previous = l.Size - diff 
+            }
+            n := l.Get(previous)
+            pScores[currentPlayer] += n.Val
+            l.RemoveAt(previous)
+            currentIndex = (previous + 1) % l.Size
+            //fmt.Printf("%v\n", currentIndex)
+            //fmt.Printf("%v played %v\n", currentPlayer, l.String())
+            currentPlayer = (currentPlayer+1) % players
+            continue
+        }
         if currentIndex == l.Size {
             l.AddAt(marble, 1)
             currentIndex = 2
@@ -28,7 +43,17 @@ func solve(){
             l.AddAt(marble, currentIndex + 1)
             currentIndex += 2
         }
+       // fmt.Printf("%v played %v\n", currentPlayer, l.String())
+        currentPlayer = (currentPlayer+1)%players
     }
+
     fmt.Printf("%v\n", l.String())
+    var max int
+    for _, v := range pScores {
+        if v > max {
+            max = v
+        }
+    }
+    fmt.Printf("%v\n", max)
 }
 
--- 2018/9/tools/tools.go
+++ 2018/9/tools/tools.go
@@ -2,7 +2,6 @@ package tools
 
 import (
     "strconv"
-    "fmt"
 )
 
 // make a doubly-linked list
@@ -16,27 +15,26 @@ type List struct {
 }
 
 type Node struct {
-    val int
+    Val int
     prev *Node
     next *Node
 }
 
-func (l *List) AddAt(value, position int) {
+func (l *List) AddAt(Value, position int) {
     if position == 0 {
-        l.AddHead(value)
+        l.AddHead(Value)
         return
     } 
     if position == l.Size {
-        l.Add(value)
+        l.Add(Value)
         return
     }
     toReplace := l.start
     for i := 0; i < position; i++ {
         toReplace = toReplace.next
     }
-    fmt.Printf("replacing: %v\n", toReplace.val)
     // now we have the node where we want to insert
-    n := &Node{value, nil, nil}
+    n := &Node{Value, nil, nil}
     before := toReplace.prev
     before.next = n
     n.prev = before
@@ -45,16 +43,16 @@ func (l *List) AddAt(value, position int) {
     l.Size++
 }
 
-func (l *List) AddHead(value int) {
+func (l *List) AddHead(Value int) {
     // push everything and add the head
-    n := &Node{value, l.start.prev, l.start}
+    n := &Node{Value, l.start.prev, l.start}
     l.start = n
     l.Size++
 }
 
-func (l *List) Add(value int) {
+func (l *List) Add(Value int) {
     if l.Size == 0 {
-        l.start = &Node{value, nil, nil}
+        l.start = &Node{Value, nil, nil}
         l.start.next = l.start
         l.start.prev = l.start
         l.Size++
@@ -63,16 +61,47 @@ func (l *List) Add(value int) {
         for i := 1; i < l.Size; i++{
             n = n.next
         }
-        n.next = &Node{value, n, l.start}
+        n.next = &Node{Value, n, l.start}
         l.Size++
     }
+}
+
+func (l *List) RemoveAt(index int) {
+    n := l.Get(index)
+    l.Remove(n)
+}
+
+func (l *List) Remove(n *Node) {
+    n.prev.next = n.next
+    n.next.prev = n.prev
+    if n == l.start {
+        l.start = n.next
+    }
+    l.Size--
+    n = nil
+}
+
+func (l *List) Get(index int) *Node {
+    n := l.start
+    for i := 0; i < index; i++ {
+        n = n.next
+    }
+    return n
+}
+
+func (l *List) FindRelative(rel, index int) *Node {
+    n := l.Get(index)
+    for i := 0; i < rel; i++ {
+        n = n.prev
+    }
+    return n
 }
 
 func (l *List) String() string {
     var output string
     n := l.start
     for i := 0; i < l.Size; i++ {
-        output += " " + strconv.Itoa(n.val)
+        output += " " + strconv.Itoa(n.Val)
         n = n.next
     }
     return output
