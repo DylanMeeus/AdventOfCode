98f603523d50cf7539e2fa051cba6641045a661e
DylanMeeus <meeusdylan@protonmail.com>
2018-12-13 02:39:47 +0100

    solution for day 12, part 2

--- 2018/12/12.go
+++ 2018/12/12.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "io/ioutil"
     "strings"
-    "sort"
 )
 
 
@@ -15,15 +14,66 @@ type rule struct {
 
 func main(){
     initial, rules := input()
-    solve(initial,rules)
+    solve2(initial,rules)
 }
 
+type plantstate struct {
+    data map[int64]bool
+}
 
-func solve(initial map[int]bool, rules []*rule) {
+func solve2(initial map[int64]bool, rules []*rule) {
     state := initial 
-    pad := func(state map[int]bool) {
+    pad := func(state map[int64]bool) {
         // find the minimum
-        var min, max int 
+        var min, max int64 
+        for k,v := range state {
+            if k < min && v {
+                min = k
+            }
+            if k > max && v {
+                max = k
+            }
+        }
+        for i := min - 5; i < max + 5;i++ {
+            state[i] = state[i] && true
+        }
+    }
+
+    sum := func(state map[int64]bool) int64 {
+        var s int64
+        for k,v := range state {
+            if v {
+                s += int64(k)
+            }
+        }
+        return s
+    }
+
+    pad(state)
+    // now we create a new state based on this one and the rules
+    for i := int64(0); i < int64(50000000000); i++ {
+        newmap := make(map[int64]bool)
+        for k,v := range state {
+           newmap[k] = calculateState(k,v, state, rules) 
+        }
+        delta := sum(newmap) - sum(state)
+        if delta == 75 { // we recurse here (by trial and error) find out our generation
+            currentScore := sum(state)
+            revisionsRemaining := 50000000000 - i
+            currentScore += (75 * revisionsRemaining)
+            fmt.Printf("%v\n", currentScore)
+            return
+        }
+        state = newmap
+        pad(state)
+    }
+}
+
+func solve(initial map[int64]bool, rules []*rule) {
+    state := initial 
+    pad := func(state map[int64]bool) {
+        // find the minimum
+        var min, max int64 
         for k,v := range state {
             if k < min && v {
                 min = k
@@ -38,15 +88,15 @@ func solve(initial map[int]bool, rules []*rule) {
     }
     pad(state)
     // now we create a new state based on this one and the rules
-    for i := 0; i < 50000000000; i++ {
-        newmap := make(map[uint]bool)
+    for i := 0; i < 20; i++ {
+        newmap := make(map[int64]bool)
         for k,v := range state {
            newmap[k] = calculateState(k,v, state, rules) 
         }
         state = newmap
         pad(state)
     }
-    var sum uint64
+    var sum int64
     for k,v := range state {
         if v {
             sum += k
@@ -56,31 +106,12 @@ func solve(initial map[int]bool, rules []*rule) {
     fmt.Printf("%v\n", sum)
 }
 
-
-func printfield(state map[int]bool) {
-    // first sort them
-    keys := []int{}
-    for k,_ := range state {
-        keys = append(keys,k)
-    }
-    sort.Ints(keys)
-    for _,k := range keys {
-        fmt.Printf(" %v", func(b bool) string {
-            if b {
-                return "#"
-            }
-            return "."
-        }(state[k]))
-    }
-    fmt.Println()
-}
-
-func calculateState(index int, alive bool, states map[int]bool, rules []*rule) bool {
+func calculateState(index int64, alive bool, states map[int64]bool, rules []*rule) bool {
     // get all rules that might apply to current one by checking the center
     for _,r := range rules {
         match := true 
         for k,v := range r.rule {
-            if states[index+k] != v {
+            if states[index+int64(k)] != v {
                match = false 
             }
         }
@@ -92,17 +123,17 @@ func calculateState(index int, alive bool, states map[int]bool, rules []*rule) b
     return false 
 }
 
-func input() (state map[int]bool, rules []*rule) {
-    state = make(map[int]bool)
+func input() (state map[int64]bool, rules []*rule) {
+    state = make(map[int64]bool)
     bytes,_ := ioutil.ReadFile("input.txt")
     parts := strings.Split(string(bytes), "\n")
     originalState := strings.Split(parts[0], " ")[2]
     for i,r := range originalState {
         if r == rune('#') {
-            state[i] = true
+            state[int64(i)] = true
         } else {
             // put it explicitely to track it easier?
-            state[i] = false
+            state[int64(i)] = false
         }
     }
 
