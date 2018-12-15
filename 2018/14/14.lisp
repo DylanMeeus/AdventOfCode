39dae5413b708534a51ea2e3b999ae4318d3eb95
DylanMeeus <meeusdylan@protonmail.com>
2018-12-15 02:07:21 +0100

    Day 14 pt 1

--- 2018/14/14.lisp
+++ 2018/14/14.lisp
@@ -1,6 +1,6 @@
 ; 14 of AoC
 
-(defparameter *loops* 10)
+(defparameter *loops* 47801)
 
 ; get the digits of a number
 (defun digits(n) 
@@ -10,16 +10,32 @@
 (defun next-index(lst pos score)
   (mod (+ pos (1+ score)) (list-length lst)))
 
-; generate recipes
-(defun generate(input iteration fst snd)
+; solve one1
+(defun sum(input recipes fst snd sumlist)
   ; just test with adding one for starters
   (let* ((x (nth fst input))
         (y (nth snd input))
         (sm (digits (+ x y))))
   (setf input (append input sm))
-  (if (= iteration *loops*)
-    input 
-    (generate input (1+ iteration) (next-index input fst x) (next-index input snd y))
+  (setf sumlist (append sumlist sm))
+  (setf recipes (+ recipes (list-length sm)))
+  (if (>= (list-length sumlist) 10)
+    (subseq sumlist 0 10)
+    (sum input recipes (next-index input fst x) (next-index input snd y) sumlist))
+  ))
+
+
+; generate recipes
+(defun generate(input recipes fst snd)
+  ; just test with adding one for starters
+  (let* ((x (nth fst input))
+        (y (nth snd input))
+        (sm (digits (+ x y))))
+  (setf input (append input sm))
+  (setf recipes (+ recipes (list-length sm)))
+  (if (>= recipes *loops*)
+    (sum input recipes (next-index input fst x) (next-index input snd y) (subseq (reverse input) 0 (- recipes *loops*)))
+    (generate input recipes (next-index input fst x) (next-index input snd y))
   )))
 
 
