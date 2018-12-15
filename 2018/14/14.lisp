863064b5641a34cab2eb92d04ceb52ac2195fa12
DylanMeeus <meeusdylan@protonmail.com>
2018-12-15 05:09:30 +0100

    slow algo

--- 2018/14/14.lisp
+++ 2018/14/14.lisp
@@ -1,6 +1,7 @@
 ; 14 of AoC
 
 (defparameter *loops* 47801)
+(defparameter *lookup* '(0 4 7 8 0 1))
 
 ; get the digits of a number
 (defun digits(n) 
@@ -38,5 +39,42 @@
     (generate input recipes (next-index input fst x) (next-index input snd y))
   )))
 
+; solve part 2
+(defun solve2(input recipes fst snd)
+  ; just test with adding one for starters
+  (let* ((x (nth fst input))
+        (y (nth snd input))
+        (sm (digits (+ x y)))
+        (index (contains (if (< (list-length input) 20)
+                           (reverse input)
+                           (subseq (reverse input) 0 10)) (reverse *lookup*) 0))
+        )
+  (setf input (append input sm))
+  (setf recipes (+ recipes (list-length sm)))
+  (print recipes)
 
+  ; check for containing substring
+  (if (= index -1)
+    (solve2 input recipes (next-index input fst x) (next-index input snd y))
+    (contains input *lookup* 0)
+    )))
+
+; return index of needle in list
+(defun contains(lst needle index) 
+    (let ((len (list-length needle)))
+      (if (or (eq lst nil) (< (list-length lst) len))
+        -1
+        (progn
+          (if (not(eq (search (subseq lst 0 len) needle) nil))
+            index
+            (contains (cdr lst) needle (1+ index))
+            )))))
+
+
+
+; solve part 1
+; (generate '(3 7) 2 0 1)
+; solve part 2
+(defun main()
+    (solve2 '(3 7) 2 0 1))
 
