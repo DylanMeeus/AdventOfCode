410d582aff7cc4fa604e77a7d31f71dd0aaf16d7
dylan <meeusdylan@protonmail.com>
2018-12-21 01:56:54 +0100

    day 20, part 1

---
+++ 2018/20/20.py
@@ -0,0 +1,37 @@
+directions = {
+        "N": (0,-1),
+        "E": (1, 0),
+        "S": (0, 1),
+        "W": (-1,0)
+    }
+
+def add(fst, snd):
+    x,y = fst[0] + snd[0], fst[1] + snd[1]
+    return (x,y)
+
+def solve(rgx): 
+    stack = []
+    current = (0,0)
+    rooms = set() 
+    groups = []
+    distance = 0
+    distances = {current: 0}
+    for char in rgx:
+        if char in "NESW":
+            # change direction
+            distance += 1
+            current = add(current, directions[char])
+            if current not in distances or distance < distances[current]:
+                distances[current] = distance
+            rooms.add(current)
+        elif char == "(":
+            groups.append((distance, current))
+        elif char == "|":
+            distance, current = groups[len(groups)-1]
+        elif char == ")":
+            groups.pop()
+    print(max(distances.values()))
+
+if __name__ == '__main__':
+    rgx = open("input.txt",'r').read()
+    solve(rgx)
