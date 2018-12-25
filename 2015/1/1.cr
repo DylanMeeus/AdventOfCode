b0734825562eb915c8f8b3650f626cfa7a9a0ba6
dylan <meeusdylan@protonmail.com>
2018-12-25 23:28:36 +0100

    2015 day 1 part2

--- 2015/1/1.cr
+++ 2015/1/1.cr
@@ -1,4 +1,16 @@
 input = File.read("input.txt")
 
 chars = input.split("").take_while{|c| c != "\n"}
+
+# solve 1
 puts chars.reduce(0){|s,c| s + (c == "(" ? 1 : -1)}
+
+# solve 2
+sum = 0
+seen = 0
+chars.each { |c|
+    sum += c == "(" ? 1 : -1
+    seen += 1
+    break if sum < 0
+}
+puts seen 
