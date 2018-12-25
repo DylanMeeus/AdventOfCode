9a24c2d43bc54541945c18ea45511fafc7950d9a
dylan <meeusdylan@protonmail.com>
2018-12-25 23:04:19 +0100

    Better part 1

--- 2015/1/1.cr
+++ 2015/1/1.cr
@@ -1,6 +1,4 @@
-
 input = File.read("input.txt")
 
 chars = input.split("").take_while{|c| c != "\n"}
-puts chars
-puts chars.map{|c| c == "(" ? 1 : -1}.sum()
+puts chars.reduce(0){|s,c| s + (c == "(" ? 1 : -1)}
