fc9b529327ba4ac1304136cd32b67ecf70e2efe8
dylan <meeusdylan@protonmail.com>
2019-12-01 13:21:52 +0100

    Solve day 1, part 2

--- 2019/1/1.hs
+++ 2019/1/1.hs
@@ -4,7 +4,10 @@ import Data.List
 f = "input.txt"
 
 fuel :: Int -> Int
-fuel x = (div x 3) - 2
+fuel x = if y > 0
+		then y
+		else 0
+	where y = (div x 3) - 2
 
 ints :: [String] -> [Int]
 ints = map read
@@ -12,8 +15,18 @@ ints = map read
 solve1 :: [Int] -> Int
 solve1 xs = sum $ map(\x -> fuel x) xs 
 
+
+fuelFM :: Int -> Int
+fuelFM x 
+ | fuel x == 0 = 0
+ | otherwise = fuel x + (fuelFM $ fuel x)
+
+solve2 :: [Int] -> Int
+solve2 xs = sum $ map(\x -> fuelFM x) xs
+
 main :: IO()
 main = do
 	content <- readFile f
 	let l = lines content
 	print $ solve1 $ ints l 
+	print $ solve2 $ ints l 
