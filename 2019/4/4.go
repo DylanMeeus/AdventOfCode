bda2136078a90f377ae8be0e6fa7208105a9c1c2
dylan <meeusdylan@protonmail.com>
2019-12-04 21:20:16 +0100

    Solve day 4, part 2

--- 2019/4/4.go
+++ 2019/4/4.go
@@ -5,11 +5,12 @@ import (
 	"strconv"
 )
 
-func main () {
+func main() {
 	fmt.Printf("%v\n", solve1())
+	fmt.Printf("%v\n", solve2())
 }
 
-func solve1() (sum int){
+func solve1() (sum int) {
 	min, max := 136760, 595730
 	for min <= max {
 		str := strconv.Itoa(min)
@@ -21,10 +22,33 @@ func solve1() (sum int){
 	return
 }
 
+func solve2() (sum int) {
+	min, max := 136760, 595730
+	for min <= max {
+		str := strconv.Itoa(min)
+		if increasing(str) && exactly2(str) {
+			sum++
+		}
+		min++
+	}
+	return
+}
+
+// exactly 2 repeating digits
+func exactly2(ss string) bool {
+	ss = "a" + ss + "a"
+	for i := 1; i < len(ss)-2; i++ {
+		if ss[i-1] != ss[i] && ss[i] == ss[i+1] && ss[i+2] != ss[i] {
+			return true
+		}
+	}
+	return false
+}
+
 func increasing(ss string) bool {
-	for i := 0; i < len(ss) - 1; i++ {
-		a, _:= strconv.Atoi(string(ss[i]))
-		b, _:= strconv.Atoi(string(ss[i+1]))
+	for i := 0; i < len(ss)-1; i++ {
+		a, _ := strconv.Atoi(string(ss[i]))
+		b, _ := strconv.Atoi(string(ss[i+1]))
 		if a > b {
 			return false
 		}
@@ -33,7 +57,7 @@ func increasing(ss string) bool {
 }
 
 func containsDouble(ss string) bool {
-	for i := 0; i < len(ss) - 1; i++ {
+	for i := 0; i < len(ss)-1; i++ {
 		if ss[i] == ss[i+1] {
 			return true
 		}
