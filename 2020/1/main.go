1ef42f905255ff2a61ebb74452a2bd0ce7e43382
Dylan Meeus <meeusdylan@protonmail.com>
2020-12-01 10:01:58 +0100

    bruteforce part 2

--- 2020/1/main.go
+++ 2020/1/main.go
@@ -3,13 +3,48 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	in, _ := ioutil.ReadFile("input.txt")
+	fmt.Printf("%v\n", solve2())
+}
 
+func getNumbers() []int {
+	in, _ := ioutil.ReadFile("input.txt")
+	nums := []int{}
+	parts := strings.Split(string(in), "\n")
+	for _, part := range parts {
+		i, err := strconv.Atoi(part)
+		if err != nil {
+			continue
+		}
+		nums = append(nums, i)
+	}
+	return nums
+}
+
+func solve2() int {
+	is := getNumbers()
+	sort.Slice(is, func(i, j int) bool { return is[i] < is[j] })
+
+	for i := 0; i < len(is); i++ {
+		for j := i + 1; j < len(is); j++ {
+			for z := j + 1; z < len(is); z++ {
+				a, b, c := is[i], is[j], is[z]
+				if a+b+c == 2020 {
+					return a * b * c
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func solve1() {
+	in, _ := ioutil.ReadFile("input.txt")
 	nums := map[int]bool{}
 	parts := strings.Split(string(in), "\n")
 	for _, part := range parts {
