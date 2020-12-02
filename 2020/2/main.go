d5c22def34dcf4b8abc6a8967a759b0fb950df42
Dylan Meeus <meeusdylan@protonmail.com>
2020-12-02 13:41:13 +0100

    Solve part 2

--- 2020/2/main.go
+++ 2020/2/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Printf("%v\n", solve1())
+	fmt.Printf("%v\n", solve2())
 }
 
 func solve1() (out int) {
@@ -35,6 +36,43 @@ func solve1() (out int) {
 		if c >= si && c <= ei {
 			out++
 		}
+	}
+	return out
+}
+
+func solve2() (out int) {
+	input := getInput()
+
+	for _, in := range input {
+		parts := strings.Split(in, ":")
+		if len(parts) != 2 {
+			continue
+		}
+		rule, pass := parts[0], parts[1]
+		pass = strings.TrimSpace(pass)
+
+		ruleParts := strings.Split(rule, " ")
+		bounds, char := ruleParts[0], ruleParts[1]
+
+		boundParts := strings.Split(bounds, "-")
+		start, end := boundParts[0], boundParts[1]
+		si, e1 := strconv.Atoi(start)
+		ei, e2 := strconv.Atoi(end)
+		if e1 != nil || e2 != nil {
+			continue
+		}
+
+		si--
+		ei--
+
+		a, b := string(pass[si]), string(pass[ei])
+		if string(a) == char && string(b) == char {
+			continue
+		}
+		if string(a) == char || string(b) == char {
+			out++
+		}
+
 	}
 	return out
 }
