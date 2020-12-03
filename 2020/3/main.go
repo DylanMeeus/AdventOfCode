47671ba651dbc58eefd159761ce810634574a22c
Dylan <meeusdylan@protonmail.com>
2020-12-03 22:32:08 +0100

    Solve part 2

--- 2020/3/main.go
+++ 2020/3/main.go
@@ -6,26 +6,50 @@ import (
 	"strings"
 )
 
-const (
-	right = 3
-)
-
 func main() {
 	fmt.Printf("%v\n", solve1())
+	fmt.Printf("%v\n", solve2())
 }
 
-func solve1() int {
+func traverse(down, right int, input []string) int {
 	trees := 0
 	xpos := 0
 
-	input := getInput()
-	for _, line := range input {
+	for i := 0; i < len(input); i += down {
+		line := input[i]
 		if string(line[xpos]) == "#" {
 			trees++
 		}
 		xpos = (xpos + right) % len(line)
 	}
+
 	return trees
+}
+
+func solve1() int {
+	input := getInput()
+	return traverse(1, 3, input)
+}
+
+func solve2() int {
+	input := getInput()
+
+	slopes := []struct {
+		right, down int
+	}{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	mult := 1
+	for _, slope := range slopes {
+		mult *= traverse(slope.down, slope.right, input)
+	}
+
+	return mult
 }
 
 func getInput() []string {
