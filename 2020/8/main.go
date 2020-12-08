c570c0aa26526aaf4834b37bf6fe96806211391d
Dylan Meeus <meeusdylan@protonmail.com>
2020-12-08 11:39:55 +0100

    solve day 8 part 2

--- 2020/8/main.go
+++ 2020/8/main.go
@@ -14,6 +14,7 @@ type instruction struct {
 
 func main() {
 	fmt.Printf("%v\n", solve1())
+	fmt.Printf("%v\n", solve2())
 }
 
 func solve1() int {
@@ -39,6 +40,49 @@ func solve1() int {
 		}
 	}
 	return reg
+}
+
+func solve2() int {
+	instructions := getInput()
+	for i, inst := range instructions {
+		original := inst
+		if inst.op == "jmp" {
+			instructions[i].op = "nop"
+		} else if inst.op == "nop" {
+			instructions[i].op = "jmp"
+		}
+		value, loop := loops(instructions)
+		if !loop {
+			fmt.Printf("%v\n", value)
+			return value
+		}
+		instructions[i] = original
+	}
+
+	return -1
+}
+
+func loops(instructions []instruction) (int, bool) {
+	iptr := 0
+	reg := 0
+	seen := map[int]bool{}
+	for iptr < len(instructions) {
+		if _, ok := seen[iptr]; ok {
+			return -1, true
+		}
+		seen[iptr] = true
+		instr := instructions[iptr]
+		switch instr.op {
+		case "acc":
+			reg += instr.value
+			iptr++
+		case "jmp":
+			iptr += instr.value
+		case "nop":
+			iptr++
+		}
+	}
+	return reg, false
 }
 
 func getInput() []instruction {
