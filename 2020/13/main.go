c6d6cfff369170c37bed95b6c3158946e6d21913
Dylan <meeusdylan@protonmail.com>
2020-12-13 16:42:16 +0100

    Solve day 13 part 2

--- 2020/13/main.go
+++ 2020/13/main.go
@@ -9,6 +9,24 @@ import (
 
 func main() {
 	fmt.Printf("%v\n", solve1())
+	fmt.Printf("%v\n", solve2())
+
+	/*
+		x := 1
+
+		start := 19
+		for {
+			part1 := ((start * x) + 2) % 37
+			part2 := ((17 * x) + 3) % 19
+
+			if res == 0 {
+				fmt.Printf("res: %v\n", x*17)
+				break
+			}
+			x++
+		}
+
+	*/
 }
 
 func solve1() int {
@@ -19,6 +37,9 @@ func solve1() int {
 	min := int(10e9)
 	minID := 0
 	for _, bus := range busses {
+		if bus == 0 {
+			continue
+		}
 		nextBus := (bus - arrival%bus)
 		if nextBus < min {
 			min = nextBus
@@ -27,6 +48,68 @@ func solve1() int {
 	}
 
 	return minID * min
+}
+
+func solve2() int {
+	// we try to look for a timestamp when two busses depart at the same time?
+	_, busses := getInput()
+
+	// we have to find a timestamp at which each bus arrives at the correct 'tick'
+	// the X means no bus needs to arrive at that time
+	// the order needs to stay the same
+
+	// we have to find an X such that (first * x) + offset = bus
+
+	//iter := 5263157894736
+
+	start := busses[0]
+
+	soFar := []int{busses[0]}
+	for _, bus := range busses[1:] {
+		time := findAllignmentTime(soFar, bus, start)
+		soFar = append(soFar, bus)
+		start = time
+	}
+	return start
+
+}
+
+func findAllignmentTime(busses []int, bus, start int) int {
+	incr := lcm(busses)
+	c := make([]int, len(busses))
+	copy(c, busses)
+	c = append(c, bus)
+	for {
+		if checkBusses(c, start) {
+			return start
+		}
+		start += incr
+	}
+
+}
+
+func lcm(busses []int) int {
+	out := 1
+	for _, bus := range busses {
+		if bus == 0 {
+			continue
+		}
+		out *= bus
+	}
+	return out
+}
+
+func checkBusses(busses []int, start int) bool {
+	for i, bus := range busses {
+		if bus == 0 {
+			continue
+		}
+		expected := start + i
+		if expected%bus != 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func getInput() (arrival int, bus []int) {
@@ -41,6 +124,7 @@ func getInput() (arrival int, bus []int) {
 
 	for _, p := range strings.Split(bs, ",") {
 		if p == "x" {
+			bus = append(bus, 0)
 			continue
 		}
 		bt, err := strconv.Atoi(p)
