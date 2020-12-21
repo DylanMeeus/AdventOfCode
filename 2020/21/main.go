812e920dd140b71f68b5f0c45df3627500eb5e6c
Dylan <meeusdylan@protonmail.com>
2020-12-21 16:57:31 +0100

    Solve day 21 part 1

---
+++ 2020/21/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+type food struct {
+	ingredients []string
+	allergens   []string
+}
+
+func main() {
+	fmt.Printf("%v\n", solve1())
+}
+
+func solve1() int {
+	foods := getInput()
+
+	// bruteforce: for each allergen, check if there is an ingredient that appears in every line
+	// with this allergen?
+
+	// for each food, find the list of possible allergens?
+	// then for each found, count the times this allergens appears?
+
+	/*
+		mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+		trh fvjkl sbzzf mxmxvkd (contains dairy)
+		sqjhc fvjkl (contains soy)
+		sqjhc mxmxvkd sbzzf (contains fish)
+
+		-> kfcds can not be fish because fish needs to be in [sgjhc, mxmvkd, sbzzf]
+	*/
+
+	// if an ingredient is missing from a line that contains the allergen, it can not be that
+	// allergen?
+
+	// for an ingredient to match an allergen, the allergen can not appear in a line where the
+	// ingredient does not appear
+
+	ingredientAllergen := map[string][]string{}
+
+	allIngs := map[string]bool{}
+	for _, food := range foods {
+		for _, ing := range food.ingredients {
+			allIngs[ing] = true
+			for _, alg := range food.allergens {
+				ingredientAllergen[ing] = append(ingredientAllergen[ing], alg)
+			}
+		}
+	}
+
+	out := 0
+	m := map[string]string{}
+	for ingredient, possibleAllergens := range ingredientAllergen {
+		for _, allergen := range possibleAllergens {
+			isMatch := true
+			for _, food := range foods {
+				// this allergen can not appear in a line where the food does not appear
+				if contains(allergen, food.allergens) && !contains(ingredient, food.ingredients) {
+					isMatch = false
+				}
+			}
+			if isMatch {
+				m[ingredient] = allergen
+			}
+		}
+	}
+
+	withoutAllergens := []string{}
+
+	for ing, _ := range allIngs {
+		if _, ok := m[ing]; !ok {
+			withoutAllergens = append(withoutAllergens, ing)
+		}
+	}
+
+	// now we count how many times they appear..
+
+	for _, food := range foods {
+		for _, ing := range food.ingredients {
+			if contains(ing, withoutAllergens) {
+				out++
+			}
+		}
+	}
+
+	return out
+}
+
+func contains(s string, ss []string) bool {
+	for _, x := range ss {
+		if s == x {
+			return true
+		}
+	}
+	return false
+
+}
+
+func getInput() []food {
+	in, _ := ioutil.ReadFile("input.txt")
+
+	foods := []food{}
+	for _, line := range strings.Split(string(in), "\n") {
+		if line == "" {
+			continue
+		}
+		f := food{}
+		ingreds := takeWhile(line, func(s string) bool { return s != "(" })
+		alergs := dropWhile(line, func(s string) bool { return s != "(" })
+		f.ingredients = nonEmpty(strings.Split(ingreds, " "))
+		alergs = alergs[len("(contains"):]
+		alergs = alergs[0 : len(alergs)-1]
+
+		f.allergens = nonEmpty(strings.Split(alergs, ","))
+
+		foods = append(foods, f)
+	}
+
+	return foods
+
+}
+
+// format the input correctly
+func nonEmpty(s []string) (out []string) {
+	for _, x := range s {
+		if len(x) != 0 {
+			out = append(out, strings.TrimSpace(x))
+		}
+	}
+	return
+}
+
+func takeWhile(s string, pred func(s string) bool) (out string) {
+	for _, c := range s {
+		if pred(string(c)) {
+			out += string(c)
+		} else {
+			return out
+		}
+	}
+	return out
+}
+
+func dropWhile(s string, pred func(s string) bool) (out string) {
+	prefix := takeWhile(s, pred)
+	return s[len(prefix):]
+}
