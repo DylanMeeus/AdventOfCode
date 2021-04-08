75340d5e7043058e18b3b03fd3f0a21cb4121b95
Dylan <meeusdylan@protonmail.com>
2021-04-08 15:46:43 +0200

    Solve 2016, day 2, part 2

--- 2016/2/src/main.rs
+++ 2016/2/src/main.rs
@@ -2,7 +2,82 @@ use std::fs;
 use std::collections::HashMap;
 
 fn main() {
-    println!("{}", solve1());
+    println!("{}", solve2());
+}
+
+fn solve2() -> String {
+    let instr = read_file();
+
+    let mut m: HashMap<(isize, isize), &str> = HashMap::new();
+    
+    // 1
+    m.insert((-2,0), "1");
+
+    // 2 3 4 
+    m.insert((-1,-1), "2");
+    m.insert((-1,0), "3");
+    m.insert((-1,1), "4");
+
+    // 5 6 7 8 9 
+    m.insert((0,-2), "5");
+    m.insert((0,-1), "6");
+    m.insert((0,0), "7");
+    m.insert((0,1), "8");
+    m.insert((0,2), "9");
+
+    // A B C 
+    m.insert((1,0), "B");
+    m.insert((1,1), "C");
+    m.insert((1,-1), "A");
+
+    // D 
+    m.insert((2,0), "D");
+
+    let mut position = (0,-2);
+
+    let mut result = String::new();
+    for line in instr.lines() {
+        for instruction in line.split("") {
+            if instruction == "U" {
+                let mut cp = position.clone();
+                cp.0 -= 1;
+                if m.contains_key(&(cp)) {
+                    position.0 -= 1;
+                }
+            }
+
+            if instruction == "D" {
+                let mut cp = position.clone();
+                cp.0 += 1;
+                if m.contains_key(&(cp)) {
+                    position.0 += 1;
+                }
+            }
+
+            if instruction == "L" {
+                let mut cp = position.clone();
+                cp.1 -= 1;
+                if m.contains_key(&(cp)) {
+                    position.1 -= 1;
+                }
+            }
+
+            if instruction == "R" {
+                let mut cp = position.clone();
+                cp.1 += 1;
+                if m.contains_key(&(cp)) {
+                    position.1 += 1;
+                }
+            }
+        }
+        // map the position to a key
+        match m.get(&position) {
+            None => println!("could not find {}", position.0),
+            Some(r) => result.push_str(r)
+        }
+    }
+
+    result
 }
 
 fn solve1() -> String {
