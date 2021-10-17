a903b72573881a01c0ad662be18eecdf4c348254
Dylan Meeus <meeusdylan@protonmail.com>
2021-10-17 22:52:26 +0200

    solve part 2 of day 5, 2016

--- 2016/5/5.ts
+++ 2016/5/5.ts
@@ -27,4 +27,38 @@ function solve1(): string {
 }
 
 
-console.log(solve1());
+function solve2(): string {
+    const input = "wtnhxymk"
+
+    let password: string[] = new Array(8);
+    
+    let i = 0;
+    let foundChars = 0;
+    while (foundChars != 8) {
+        let foundHash = false;
+        while (!foundHash) {
+            const candidate = input + i;
+            i++;
+            const hashed = Md5.hashStr(candidate);
+            if ( hashed.startsWith("00000") ){
+                if ("01234567".indexOf(hashed.toString()[5]) < 0) {
+                    continue;
+                }
+                const  idx = + hashed.toString()[5];
+                if (password[idx] !== undefined || idx > 7) {
+                    continue;
+                }
+                const char = hashed.toString()[6];
+                foundHash = true;
+                foundChars++;
+                password[idx] = char;
+                console.log(password.join(''));
+            }
+        }
+
+    }
+
+    return password.join('');
+}
+
+console.log(solve2());
