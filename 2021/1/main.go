16ae3c6d07a3790c3301d34c614a34dceed04bc4
Dylan Meeus <meeusdylan@protonmail.com>
2021-12-02 01:54:36 +0100

    solve day 1 part 2

--- 2021/1/main.go
+++ 2021/1/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Printf("%v\n", solve())
+	fmt.Printf("%v\n", solve2())
 }
 
 func getData() (out []int) {
@@ -33,6 +34,22 @@ func solve() (sum int) {
 	for i := 1; i < len(data); i++ {
 		if data[i] > data[i-1] {
 
+			sum++
+		}
+	}
+	return sum
+}
+
+func solve2() (sum int) {
+	defer func() {
+		recover()
+	}()
+
+	data := getData()
+	for i := 0; i < len(data); i++ {
+		a := data[i] + data[i+1] + data[i+2]
+		b := data[i+1] + data[i+2] + data[i+3]
+		if b > a {
 			sum++
 		}
 	}
