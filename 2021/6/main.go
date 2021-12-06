37142fa6c210ae88096a31a86a9c257d296ff52c
Dylan Meeus <meeusdylan@protonmail.com>
2021-12-06 23:49:15 +0100

    solve day 6 part 1

---
+++ 2021/6/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type Fish struct {
+	Timer int
+}
+
+func (f *Fish) Tick() bool {
+
+	if f.Timer == 0 {
+		f.Timer = 6
+		return true
+	}
+
+	f.Timer--
+	return false
+}
+
+func spawn() *Fish {
+	return &Fish{Timer: 8}
+}
+
+func main() {
+	fmt.Printf("%v\n", solve())
+}
+
+func getData() []Fish {
+
+	input, _ := ioutil.ReadFile("./input.txt")
+
+	in := strings.Replace(string(input), "\n", "", -1)
+	stringFishParts := strings.Split(in, ",")
+
+	school := make([]Fish, len(stringFishParts))
+	for i, timer := range stringFishParts {
+		intTimer, err := strconv.Atoi(timer)
+		if err != nil {
+			panic(err)
+		}
+		school[i] = Fish{Timer: intTimer}
+	}
+
+	return school
+}
+
+func solve() int {
+	data := getData()
+	school := simulate(data, 80)
+	return len(school)
+}
+
+func copyMutable(school []Fish) []*Fish {
+	mutableSchool := make([]*Fish, len(school))
+	for i := 0; i < len(school); i++ {
+		mutableSchool[i] = &school[i]
+	}
+	return mutableSchool
+}
+
+func simulate(initialSchool []Fish, days int) []*Fish {
+	school := copyMutable(initialSchool)
+	for day := 0; day < days; day++ {
+
+		for _, fish := range school {
+			if givesBirth := fish.Tick(); givesBirth {
+				school = append(school, spawn())
+			}
+		}
+
+	}
+
+	return school
+}
