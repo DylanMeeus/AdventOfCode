4ef0858e6259a483e7a9e9569a6b52944034be48
Dylan Meeus <meeusdylan@protonmail.com>
2021-12-10 01:36:53 +0100

    solve day 9 part 2

--- 2021/9/main.go
+++ 2021/9/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type heightmap [][]int
+
+func (h heightmap) print() {
+	for row := 0; row < len(h); row++ {
+		for _, cell := range h[row] {
+			fmt.Printf("%v\t", cell)
+		}
+		fmt.Println()
+	}
+}
 
 func (h heightmap) valueAtPoint(p point) (int, error) {
 	if p.row < 0 || p.row >= len(h) {
@@ -25,6 +35,7 @@ type point struct{ row, col int }
 
 func main() {
 	fmt.Printf("%v\n", solve())
+	fmt.Printf("%v\n", solve2())
 }
 
 func getData() heightmap {
@@ -61,6 +72,63 @@ func solve() int {
 	return sum
 }
 
+func solve2() int {
+	data := getData()
+
+	identifier := -1
+	basinSize := map[int]int{}
+	for row := 0; row < len(data); row++ {
+		for col := 0; col < len(data[row]); col++ {
+			floodFill(data, point{row, col}, identifier, basinSize)
+			identifier--
+		}
+		identifier--
+	}
+
+	values := sortMapValues(basinSize)
+
+	mult := 1
+	for _, v := range values[:3] {
+		mult *= v
+	}
+
+	return mult
+}
+
+func sortMapValues(m map[int]int) []int {
+	out := []int{}
+	for _, v := range m {
+		out = append(out, v)
+	}
+	sort.Slice(out, func(i, j int) bool { return out[i] > out[j] })
+	return out
+}
+
+func floodFill(data heightmap, p point, identifier int, basinSize map[int]int) {
+	if value, err := data.valueAtPoint(p); err == nil {
+		if value == 9 {
+			return
+		}
+		if value < 0 {
+			// this has already been explored
+			return
+		}
+		data[p.row][p.col] = identifier
+		basinSize[identifier]++
+	} else {
+		return
+	}
+	ns := newNeighbours(p)
+	for _, neigh := range ns {
+		if val, err := data.valueAtPoint(neigh); err != nil {
+			if val == 9 {
+				continue
+			}
+		}
+		floodFill(data, neigh, identifier, basinSize)
+	}
+}
+
 // getLowPoints returns all points for the heightmap
 // which are lower than their neighbours
 func getLowPoints(h heightmap) []point {
@@ -95,6 +163,15 @@ func all(ps []point, pred func(point) bool) bool {
 		}
 	}
 	return true
+}
+
+func newNeighbours(p point) []point {
+	ns := []point{}
+	ns = append(ns, point{p.row - 1, p.col})
+	ns = append(ns, point{p.row + 1, p.col})
+	ns = append(ns, point{p.row, p.col - 1})
+	ns = append(ns, point{p.row, p.col + 1})
+	return ns
 }
 
 func neighbours(p point) []point {
