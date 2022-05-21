f4be9ce676d3d853d9a3ce228f95ef0cc146e3a4
Dylan Meeus <meeusdylan@protonmail.com>
2022-05-21 15:39:45 +0200

    Solve day 3 part 2

--- 2015/3/main.go
+++ 2015/3/main.go
@@ -11,6 +11,7 @@ type point struct {
 
 func main() {
 	fmt.Printf("%v\n", solve())
+	fmt.Printf("%v\n", solve2())
 }
 
 func solve() int {
@@ -36,12 +37,40 @@ func solve() int {
 	return len(m)
 }
 
+func solve2() int {
+	in := getInput()
+
+	santa := &point{0, 0}
+	robot := &point{0, 0}
+
+	funcMap := map[string]func(p *point){
+		"^": func(p *point) { p.y-- },
+		"v": func(p *point) { p.y++ },
+		">": func(p *point) { p.x++ },
+		"<": func(p *point) { p.x-- },
+	}
+
+	m := map[point]int{point{santa.x, santa.y}: 1}
+	for i, c := range in {
+		if f, ok := funcMap[string(c)]; ok {
+			if i%2 == 0 {
+				f(santa)
+			} else {
+				f(robot)
+			}
+			m[point{santa.x, santa.y}]++
+			m[point{robot.x, robot.y}]++
+		}
+	}
+
+	return len(m)
+}
+
 func getInput() string {
 	b, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%v\n", string(b))
 	return string(b)
 }
