197edea2093214452d93ccfe8eb2d9ca8fb1e8a0
Dylan Meeus <meeusdylan@protonmail.com>
2022-05-22 15:20:21 +0200

    solve test problem

--- 2015/7/main.go
+++ 2015/7/main.go
@@ -17,16 +17,26 @@ const (
 	LSHIFT
 )
 
+var (
+	gateTypeMapping = map[string]gateType{
+		"AND":    AND,
+		"OR":     OR,
+		"NOT":    NOT,
+		"RSHIFT": RSHIFT,
+		"LSHIFT": LSHIFT,
+	}
+)
+
 type wire struct {
 	name  string
-	value int16
+	value uint16
 }
 
 type gate struct {
 	input        []*wire
 	output       *wire
 	operation    gateType
-	initialValue int16 // for rshift / lshift modiifiers
+	initialValue uint16 // for rshift / lshift modifiers, so this is optional
 }
 
 type node struct {
@@ -43,12 +53,35 @@ func main() {
 
 func solve() {
 	wires, gates := parseToWireAndGate(getInput())
+	_ = wires
+	//printWires(wires)
+	//printGates(gates)
+
+	// solve for the gates..
+
+	for _, gate := range gates {
+		// try to resolve it
+		if gate.output.value == 0 {
+			switch gate.operation {
+			case AND:
+				gate.output.value = gate.input[0].value & gate.input[1].value
+			case OR:
+				gate.output.value = gate.input[0].value | gate.input[1].value
+			case RSHIFT:
+				gate.output.value = gate.input[0].value >> gate.initialValue
+			case LSHIFT:
+				gate.output.value = gate.input[0].value << gate.initialValue
+			case NOT:
+				gate.output.value = ^gate.input[0].value // XOR with FF to get bitwise NOT
+			}
+		}
+	}
+
 	printWires(wires)
-	_ = gates
 }
 
 func getInput() []string {
-	bts, err := ioutil.ReadFile("./test_input.txt")
+	bts, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -68,9 +101,23 @@ func getWire(ws []*wire, name string) *wire {
 // for debugging stuff
 func printWires(ws []*wire) {
 	for _, w := range ws {
-		fmt.Printf("%v,", w.name)
+		fmt.Printf("(%v %v) ", w.name, w.value)
 	}
 	fmt.Println()
+}
+
+func printGates(gs []*gate) {
+	for _, g := range gs {
+		fmt.Printf("BEGIN\n")
+		fmt.Print("(")
+		fmt.Printf("input wires: ")
+		printWires(g.input)
+		fmt.Printf("output wires: ")
+		printWires([]*wire{g.output})
+		fmt.Printf("operator: %v\n", g.operation)
+		fmt.Printf("init: %v\n", g.initialValue)
+		fmt.Printf("END \n\n")
+	}
 }
 
 func createWires(lines []string) []*wire {
@@ -83,11 +130,12 @@ func createWires(lines []string) []*wire {
 		parts := strings.Split(line, " ")
 		isInput := len(parts) == 3
 		if isInput {
+			fmt.Println(line)
 			value, err := strconv.Atoi(parts[0])
 			if err != nil {
 				panic(err)
 			}
-			wires = append(wires, &wire{name: parts[2], value: int16(value)})
+			wires = append(wires, &wire{name: parts[2], value: uint16(value)})
 		} else {
 			for _, part := range parts {
 				isOperator := part == "NOT" || part == "RSHIFT" || part == "LSHIFT" || part == "AND" || part == "OR"
@@ -108,23 +156,19 @@ func createWires(lines []string) []*wire {
 	return wires
 }
 
-func parseToWireAndGate(lines []string) (wires []*wire, gates []gate) {
-	wires = createWires(lines)
-	return
-	gates = []gate{}
+// createGates constructs the actual logic gates. This expects an exhaustive list of all wires to be
+// passed in.
+func createGates(lines []string, wires []*wire) []*gate {
+	gates := []*gate{}
+
 	for _, line := range lines {
 		if line == "" {
 			continue
 		}
 
 		parts := strings.Split(line, " ")
-		isInput := len(parts) == 3
-		if isInput {
-			value, err := strconv.Atoi(parts[0])
-			if err != nil {
-				panic(err)
-			}
-			wires = append(wires, &wire{name: parts[2], value: int16(value)})
+		if len(parts) == 3 {
+			continue
 		} else if len(parts) == 5 {
 			// this is an "operator" mapping
 			OP := parts[1]
@@ -132,32 +176,62 @@ func parseToWireAndGate(lines []string) (wires []*wire, gates []gate) {
 			case "OR":
 				fallthrough
 			case "AND":
-				break
-				/*
-					nodeName := parts[1] + parts[0] + parts[2]
-					graph[parts[0]] = append(graph[parts[0]], nodeName)
-					graph[parts[2]] = append(graph[parts[2]], nodeName)
-					graph[nodeName] = []string{parts[4]}
-				*/
+				op := gateTypeMapping[OP]
+				wireIn1 := getWire(wires, parts[0])
+				wireIn2 := getWire(wires, parts[2])
+				wireOut := getWire(wires, parts[4])
+				gates = append(gates, &gate{
+					input:        []*wire{wireIn1, wireIn2},
+					output:       wireOut,
+					operation:    op,
+					initialValue: 0,
+				})
 			case "LSHIFT":
 				fallthrough
 			case "RSHIFT":
-				break
-				/*
-					nodeName := parts[1] + parts[0] + parts[2]
-					graph[parts[0]] = append(graph[parts[0]], nodeName)
-					graph[parts[2]] = append(graph[parts[2]], nodeName)
-					graph[nodeName] = []string{parts[3]}
-				*/
+				op := gateTypeMapping[OP]
+				wireIn1 := getWire(wires, parts[0])
+				wireOut := getWire(wires, parts[4])
+				initialValue, err := strconv.Atoi(parts[2])
+				panicIfErr(err)
+				gates = append(gates, &gate{
+					input:        []*wire{wireIn1},
+					output:       wireOut,
+					operation:    op,
+					initialValue: uint16(initialValue),
+				})
 			}
+		} else {
+			op := gateTypeMapping[parts[0]]
+			wireIn1 := getWire(wires, parts[1])
+			wireOut := getWire(wires, parts[3])
+			gates = append(gates, &gate{
+				input:        []*wire{wireIn1},
+				output:       wireOut,
+				operation:    op,
+				initialValue: 0,
+			})
+
 		}
 	}
+	return gates
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parseToWireAndGate(lines []string) (wires []*wire, gates []*gate) {
+	wires = createWires(lines)
+	gates = createGates(lines, wires)
 	return
 }
 
-func parseInput(in []string) (graph map[string][]string, inputs map[string]int16) {
+func parseInput(in []string) (graph map[string][]string, inputs map[string]uint16) {
 	graph = map[string][]string{}
-	inputs = map[string]int16{}
+	inputs = map[string]uint16{}
 	for _, line := range in {
 		if line == "" {
 			continue
@@ -170,7 +244,7 @@ func parseInput(in []string) (graph map[string][]string, inputs map[string]int16
 			if err != nil {
 				panic(err)
 			}
-			inputs[parts[2]] = int16(value)
+			inputs[parts[2]] = uint16(value)
 		} else if len(parts) == 5 {
 			// this is an "operator" mapping
 			OP := parts[1]
