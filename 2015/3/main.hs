0a2f06c21cd8c6cbb80cdf47e73c0c5d133c531d
Dylan Meeus <meeusdylan@protonmail.com>
2024-06-08 12:38:11 -0400

    solve 2015, day3, pt2

--- 2015/3/main.hs
+++ 2015/3/main.hs
@@ -33,12 +33,21 @@ walk (x:xs) current =
     in newPos : (walk xs newPos)
 
 solve1 :: String -> Int
-solve1 input  = (length . nub) $ (pos : (walk input pos))
+solve1 input  = (length . nub) (pos : (walk input pos))
 
+--solve2 :: String -> Int
+solve2 input = 
+    let santa_input = [input !! idx | idx <- [0,2..length input]]
+        robo_santa_input = [input !! idx | idx <- [1,3..(length input) - 1]]
+        all_pos = (pos : walk santa_input pos) <> (pos : walk robo_santa_input pos)
+    in (length . nub) all_pos
+        
 
 main :: IO ()
 main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let result1 = solve1 contents
+    let result2 = solve2 contents
     putStrLn $ show result1
+    putStrLn $ show result2
