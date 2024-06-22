cfddb1dcf708a2c8e1930dffdaa2ba97cab1be1b
Dylan Meeus <meeusdylan@protonmail.com>
2024-06-22 11:48:57 -0400

    solve 2015, day 5, pt2

--- 2015/5/main.hs
+++ 2015/5/main.hs
@@ -28,11 +28,56 @@ isNice s = (threeVowels s && doubleLetter s) && (not $ isBlocked s)
 solve1 :: [String] -> Int
 solve1 input = length $ filter(\s -> isNice s) input
 
+
+-- problem 2
+
+countOccurrences :: (Ord a) => [a] -> [(a, Int)]
+countOccurrences xs = map(\l -> (head l, length l)) . group . sort $ xs
+
+indexes :: Int -> String -> String -> [Int]
+indexes acc stack needle 
+    | stack == "" = []
+    | isPrefixOf needle stack = [acc] <> indexes (succ acc) (tail stack) needle
+    | otherwise = indexes (succ acc) (tail stack) needle
+
+
+hasNonOverlappingIndex :: Int -> [Int] -> Bool
+hasNonOverlappingIndex i lst = 1 <= length (filter(\x -> x /= i && x /= (succ i) && x /= (pred i)) lst)
+
+findNonOverlappingIndexes :: [Int] -> [Int]
+findNonOverlappingIndexes list = filter(\l -> hasNonOverlappingIndex l list) list
+
+findDuplicatePairs :: String -> [String]
+findDuplicatePairs "" = []
+findDuplicatePairs input = 
+    let zipped = tail $ init $ zip (" " <> input) (input <> " ")
+        instances = countOccurrences zipped
+    in map(\i -> [fst (fst i)] ++ [snd (fst i)]) $ filter(\i -> (snd i) > 1) instances
+
+hasValidPairs :: String -> Bool
+hasValidPairs s = 
+    let pairs = map (\pair -> indexes 0 s pair) (findDuplicatePairs s)
+        idx = map (\pair -> findNonOverlappingIndexes pair) pairs
+        valid = length $ filter (\x -> x /= []) idx
+    in valid >= 1
+
+
+validRepetition :: String -> Bool
+validRepetition s = 
+    let idx = [x | x <- [0..(pred $ length s)]]
+    in 1 <= length (filter(\y -> s !! y == s !! (y+2)) (init $ init idx))
+        
+
+solve2 :: [String] -> Int
+solve2 input = length $ filter(\s -> (hasValidPairs s) && validRepetition s) input
+
 main :: IO () 
 main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let input = lines contents
     let result1 = solve1 input
+    let result2 = solve2 input
     putStrLn $ show result1
+    putStrLn $ show result2
     hClose handle
