45ef3cfe9fea3e57201678059cdc4bde57d604f1
Dylan Meeus <meeusdylan@protonmail.com>
2024-12-24 15:06:23 -0500

    partial solve for pt 2, day 5 2024

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -28,11 +28,58 @@ def solve1(rules, pages) -> int:
     return result
 
 
+def no_edge(S, G) -> int:
+    for value in S:
+        if not G in 
+
+
+def fix_page_set(page_set, rules) -> [int]:
+    # construct the sub-graph first
+    G = {} 
+    
+    nub_values = set()
+
+    for page in page_set:
+        nub_values.add(page)
+        if page in rules:
+            G[page] = []
+            for value in rules[page]:
+                if value in page_set:
+                    G[page].append(value)
+
+    # now build the page_set in reverse against G, by finding the node with no "After" (edge)
+    # then delete this node from all rules.. 
+
+
+    return page_set
+
+
+def solve2(rules, pages) -> int:
+    """ build a graph from the 'end' node, and delete each consumed node.. until N/2 nodes are deleted """ 
+    result = 0
+
+    for page_set in pages:
+        valid_page_set = True
+        seen = set()
+        for page in page_set:
+            if page in rules:
+                applicable_rule = rules[page]
+                if not validate_page(applicable_rule, seen, page_set):
+                    valid_page_set = False
+            seen.add(page)
+        if not valid_page_set:
+            page_set = fix_page_set(page_set, rules)
+            mid = len(page_set) // 2
+            result += page_set[mid]
+
+
+
+    return result
 
 
 def get_input():
     # parse out the input in 'rules' and 'actual input'
-    lines = open('input.txt').read().split('\n')
+    lines = open('test_input.txt').read().split('\n')
 
     pages = []
     # rules encodes a key (after) a value (before)
@@ -61,4 +108,5 @@ if __name__ == '__main__':
     r, p = get_input()
     print(r)
     print(solve1(r,p))
+    print(solve2(r,p))
 
