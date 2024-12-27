5cd814f8a2fcb53483eb4ad473ebc482d318e0b5
Dylan Meeus <meeusdylan@protonmail.com>
2024-12-26 20:17:06 -0500

    2024-7-1

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -32,6 +32,21 @@ def can_solve(t, c, acc) -> bool:
 
 
 
+def can_solve2(t, c, acc) -> bool:
+
+    if len(c) == 0 and acc == t: 
+        return True
+
+    if len(c) == 0 or acc > t:
+        return False
+
+    to_consume = c[0]
+    remainder = c[1:]
+
+    str_concat = str(acc) + str(to_consume)
+    return can_solve2(t, remainder, acc + to_consume) or can_solve2(t, remainder, acc * to_consume) or can_solve2(t, remainder, int(str_concat))
+
+
 
 def solve1(data) -> int:
     result = 0
@@ -40,6 +55,14 @@ def solve1(data) -> int:
             result += eq.target
     return result
 
+def solve2(data) -> int:
+    result = 0
+    for eq in data:
+        if can_solve2(eq.target, eq.components[1:], eq.components[0]):
+            result += eq.target
+    return result
+
 if __name__ == '__main__':
     data = get_input()
     print(solve1(data))
+    print(solve2(data))
