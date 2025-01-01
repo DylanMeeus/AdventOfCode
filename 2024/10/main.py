4be2128dc2d71c86a04c5b390748f92129ca131e
Dylan Meeus <meeusdylan@protonmail.com>
2025-01-01 14:33:52 -0500

    2024-10-1

---
+++ 2024/10/main.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from typing import Dict, Tuple, Set
+
+
+@dataclass(frozen=True)
+class Point:
+    row: int
+    col: int
+
+
+def get_input() -> Tuple[Dict[Point, int], Set[Point]]:
+    lines = open('input.txt').read().split('\n')
+    out = {}
+    zeroes = set()
+    for row, line in enumerate(lines):
+        for col, height in enumerate(line):
+            p = Point(row,col)
+            out[p] = int(height)
+            if int(height) == 0 :
+                zeroes.add(p)
+    return out, zeroes
+
+
+
+def crawl(data: Dict[Point, int], start: Point) -> int:
+    nines: Set[Point] = set()
+    def inner_crawl(current_point: Point, seen: Set[Point]):
+        if current_point not in data:
+            return
+        if data[current_point] == 9:
+            nines.add(current_point)
+            return
+
+        seen.add(current_point)
+        # else we need to look in all three directions .. 
+        left = Point(current_point.row, current_point.col - 1)
+        right = Point(current_point.row, current_point.col + 1)
+        up = Point(current_point.row - 1, current_point.col)
+        down = Point(current_point.row + 1, current_point.col)
+
+
+        if left not in seen and left in data and (data[left] - data[current_point] == 1):
+            inner_crawl(left, seen)
+        if right not in seen and right in data and (data[right] - data[current_point]) == 1:
+            inner_crawl(right, seen)
+        if up not in seen and up in data and (data[up] - data[current_point]) == 1:
+            inner_crawl(up, seen)
+        if down not in seen and down in data and (data[down] - data[current_point]) == 1:
+            inner_crawl(down, seen)
+
+    inner_crawl(start, set())
+    return len(nines)
+
+
+
+def solve1(data: Dict[Point,int], zeroes: Set[Point]) -> int:
+    result = 0
+
+    for zero in zeroes:
+        nines = crawl(data, zero)
+        result += nines
+
+    return result
+
+
+if __name__ == '__main__':
+    data, zeroes = get_input()
+    print(solve1(data,zeroes))
