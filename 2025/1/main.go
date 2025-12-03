d7236d279a61438d0e2c894491b7fc52bcfc6efa
Dylan Meeus <meeusdylan@protonmail.com>
2025-12-02 23:05:35 -0500

    solve day1 pt2

--- 2025/1/main.go
+++ 2025/1/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -22,6 +21,7 @@ func main() {
 	fmt.Println(len(is))
 	fmt.Println("-----")
 
+	//fmt.Println(countRollOvers(99, -99))
 	/*
 		is = []instr{
 			{
@@ -32,8 +32,37 @@ func main() {
 			},
 		}
 	*/
+
+	fmt.Println(getPos(99, 1))
+	fmt.Println(getPos(1, -1))
+
+	fmt.Println(countRollOvers(0, -200))
+
 	//fmt.Println(solve1(is))
-	fmt.Println(solve2(is))
+	//fmt.Println(solve2(is))
+
+	moves := MapToMoves(is)
+	fmt.Println(solveAlt(moves))
+}
+
+func solveAlt(moves []int) int {
+	pos := 50
+
+	c := 0
+	for _, move := range moves {
+		pos += move
+		if pos == -1 {
+			pos = 99
+		}
+		if pos == 101 {
+			pos = 0
+		}
+		if pos == 0 {
+			c++
+		}
+	}
+
+	return c
 }
 
 func solve1(is []instr) int {
@@ -57,42 +86,104 @@ func solve1(is []instr) int {
 	return m[0]
 }
 
+func getPos(pos, moves int) int {
+
+	if moves > 0 {
+		return (pos + moves) % DIAL_LENGTH
+	}
+
+	moves *= -1
+	nextPos := pos - moves
+	fmt.Println(nextPos)
+	if nextPos < 0 {
+		// how many times can we loop around back to 0?
+		remainingMoves := moves - pos
+		fmt.Printf("remaining moves: %v\n", remainingMoves)
+		loops := remainingMoves / DIAL_LENGTH
+		remainingMoves = remainingMoves - (loops * DIAL_LENGTH)
+		return DIAL_LENGTH - remainingMoves
+	} else {
+		return nextPos
+	}
+
+	return -1337
+}
+
+func countRollOvers(pos int, moves int) int {
+	if moves > 0 {
+		// remMoves is how to calculate it yo
+
+		if pos == 0 && moves < DIAL_LENGTH {
+			return 0
+		}
+
+		distToL := (DIAL_LENGTH) - pos
+		remMoves := moves - distToL
+		if remMoves < 0 {
+			// can't reach the end of the dial
+			return 0
+		}
+
+		if remMoves == 0 {
+			return 1
+		}
+
+		return 1 + (remMoves / DIAL_LENGTH)
+	} else {
+
+		// how often can it go negative..
+		// how many positional moves left after we go to 0
+		moves *= -1
+		if pos == 0 && moves < DIAL_LENGTH {
+			return 0
+		}
+		if moves < pos {
+			return 0
+		}
+		remMoves := moves - pos
+		if remMoves == 0 {
+			return 1
+		}
+		return 1 + (remMoves / DIAL_LENGTH)
+	}
+	return 0
+}
+
+func MapToMoves(is []instr) []int {
+	out := []int{}
+	for _, i := range is {
+		input := 1
+		if i.dir == "L" {
+			input = -1
+		}
+		for j := 0; j < i.val; j++ {
+			out = append(out, input)
+		}
+	}
+	return out
+}
+
 func solve2(is []instr) int {
 	pos := 50
 	c := 0
 	for _, inst := range is {
 		var nextPos int
 		if inst.dir == "R" {
-			nextPos = (pos + inst.val) % DIAL_LENGTH
-			rollOver := (pos + inst.val) / DIAL_LENGTH
-			fmt.Printf("roll over: %v\n", rollOver)
-			c += rollOver
+			nextPos = getPos(pos, inst.val)
+			count := countRollOvers(pos, inst.val)
+			c += count
+			fmt.Printf("passed 0 %v times going from %v to %v in %v moves\n", count, pos, nextPos, inst.val)
 		} else if inst.dir == "L" {
-			nextPos = (pos - inst.val)
-			if nextPos == 0 {
-				c++
-			} else if nextPos < 0 {
-				if pos != 0 {
-					v := pos - inst.val
-					div := math.Abs(float64(v / DIAL_LENGTH))
-					if int(div) > 0 {
-						fmt.Printf("roll over (L): %v\n", div)
-						c += int(div) + 1
-					} else {
-						fmt.Printf("roll over (L): %v\n", 1)
-						c++
-					}
-				}
-				rem := nextPos % DIAL_LENGTH
-				nextPos = DIAL_LENGTH + rem
-			}
-			// figure out how much smaller than 0
+			count := countRollOvers(pos, -inst.val)
+			c += count
+			nextPos = getPos(pos, -inst.val)
+
+			fmt.Printf("--passed 0 %v times going from %v to %v in %v moves\n", count, pos, nextPos, inst.val)
 		} else {
 			panic("should not happen yo")
 		}
 
 		pos = nextPos
-		fmt.Println(pos)
 	}
 	return c
 }
