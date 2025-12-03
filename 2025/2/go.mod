1ae54740cb773a5a927a406088089a2b1f3ede61
Dylan Meeus <meeusdylan@protonmail.com>
2025-12-03 00:15:32 -0500

    solve day2 pt 2

---
+++ 2025/2/go.mod
@@ -0,0 +1,3 @@
+module github.com/DylanMeeus/AdventOfCode/2025/2
+
+go 1.25.4
--- 2025/2/main.go
+++ 2025/2/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	numbers := parseInput(readInput())
+	//fmt.Println(solve1(numbers))
+
 	//numbers = []int{1010}
-	fmt.Println(solve1(numbers))
+	fmt.Println(solve2(numbers))
 }
 
 func solve1(is []int) int {
-
 	c := 0
 
 	for _, number := range is {
@@ -28,6 +30,30 @@ func solve1(is []int) int {
 		if a == b {
 			fmt.Println(number)
 			c += number
+		}
+	}
+
+	return c
+}
+
+func solve2(is []int) int {
+	c := 0
+
+outer:
+	for _, number := range is {
+		strNum := strconv.Itoa(number)
+		// find all possible sequences and see if they repeat..
+		for i := 0; i < (len(strNum) / 2); i++ {
+			for j := i + 1; j < len(strNum); j++ {
+				sequence := strNum[i:j]
+				re := regexp.MustCompile(fmt.Sprintf("(%s){%d}", sequence, len(strNum)/len(sequence)))
+				matches := re.FindString(strNum) == strNum
+				//fmt.Printf("checking sequence: %v against %v\n", strNum, re)
+				if matches {
+					c += number
+					continue outer
+				}
+			}
 		}
 	}
 
