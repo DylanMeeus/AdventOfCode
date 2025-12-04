a020a1f13ebf02c034c0064851c6b58485f19789
Dylan Meeus <meeusdylan@protonmail.com>
2025-12-03 23:40:18 -0500

    day 3 pt 2 partial

---
+++ 2025/3/go.mod
@@ -0,0 +1,3 @@
+module github.com/DylanMeeus/AdventOfCode/2025/3
+
+go 1.25.4
--- 2025/3/main.go
+++ 2025/3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -10,6 +11,16 @@ import (
 func main() {
 	lines := readInput()
 	fmt.Println(solve1(lines))
+
+	f := (findMax12("987654321111111"))
+	fmt.Println(f)
+	fmt.Println(len(strconv.Itoa(f)))
+	f = (findMax12("234234234234278"))
+	fmt.Println(f)
+	fmt.Println(len(strconv.Itoa(f)))
+	n := lineToTree("1234")
+	printTree(n, "")
+	fmt.Println(solve2(lines))
 }
 
 // find the max of 2 batteries combined
@@ -31,6 +42,60 @@ func findMax(line string) int {
 	return max
 }
 
+type Node struct {
+	Value          int
+	DepthRemaining int
+	Children       []*Node
+}
+
+func findMax12(line string) int {
+	max := 0
+	root := lineToTree(line)
+	// now find the path with max value, using max 12 nodes..
+	var nodeMax func(*Node, int, int)
+	nodeMax = func(start *Node, currentValue, remainder int) {
+		if start == nil {
+			return
+		}
+		if start.DepthRemaining < remainder {
+			return
+		}
+		if remainder == 0 {
+			nodeValue := start.Value * int(math.Pow(10., float64(remainder)))
+			currentValue += nodeValue
+			if currentValue > max {
+				max = currentValue
+			}
+			return
+		}
+		nodeValue := start.Value * int(math.Pow(10., float64(remainder)))
+		// take each of the children
+		for _, child := range start.Children {
+			nodeMax(child, currentValue+nodeValue, remainder-1)
+			if start.DepthRemaining > 11 {
+				nodeMax(child, 0, 11) // also start a search from the child onwwards
+			}
+		}
+	}
+	nodeMax(root, 0, 11)
+	return max
+}
+
+func lineToTree(line string) *Node {
+	if line == "" {
+		return nil
+	}
+	val, err := strconv.Atoi(string(line[0]))
+	if err != nil {
+		panic(err)
+	}
+	node := &Node{Value: val, DepthRemaining: len(line) - 1}
+	for i := 1; i < len(line); i++ {
+		node.Children = append(node.Children, lineToTree(line[i:]))
+	}
+	return node
+}
+
 func solve1(lines []string) int {
 	t := 0
 
@@ -41,10 +106,29 @@ func solve1(lines []string) int {
 	return t
 }
 
+func solve2(lines []string) int {
+	t := 0
+
+	for i, line := range lines {
+		t += findMax12(line)
+		fmt.Printf("parsed %v of %v lines\n", i, len(lines))
+	}
+
+	return t
+}
 func readInput() []string {
 	b, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	return strings.Split(string(b), "\n")
+}
+
+func printTree(n *Node, start string) {
+
+	fmt.Printf("%v node value: %v, D rem: %v\n", start, n.Value, n.DepthRemaining)
+	for _, c := range n.Children {
+		printTree(c, start+"-")
+	}
+
 }
