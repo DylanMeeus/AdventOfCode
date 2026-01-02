68d822318a53f8281120eafe24fb4ec44b2df35b
Dylan Meeus <meeusdylan@protonmail.com>
2026-01-01 20:03:50 -0500

    incremental stuff

--- 2025/10/main.go
+++ 2025/10/main.go
@@ -7,10 +7,124 @@ import (
 	"strings"
 )
 
+type LinearProblem struct {
+	Inputs [][][]int
+	Output [][]int
+	N      int
+}
+
+func (l LinearProblem) solver() int {
+	// minimize the multipliers?
+
+	return 0
+}
+
+func print2d(is [][]int) {
+	N := len(is)
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			fmt.Printf(" %v ", is[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func toMatrix(input []int, N int) [][]int {
+	output := make([][]int, N)
+	for i := 0; i < N; i++ {
+		row := make([]int, N)
+		for j := 0; j < N; j++ {
+			if i == j {
+				row[j] = input[j]
+			} else {
+				row[j] = 0
+			}
+		}
+		output[i] = row
+	}
+	return output
+}
+
+func contains(i int, input []int) bool {
+	for _, in := range input {
+		if i == in {
+			return true
+		}
+	}
+	return false
+}
+
+func toInputMatrix(input []int, N int) [][]int {
+	output := make([][]int, N)
+	for i := 0; i < N; i++ {
+		row := make([]int, N)
+		for j := 0; j < N; j++ {
+			if i == j && contains(j, input) {
+				row[j] = 1
+			} else {
+				row[j] = 0
+			}
+		}
+		output[i] = row
+	}
+	return output
+}
+
+func (m Machine) toLP() LinearProblem {
+	N := len(m.joltage)
+	output := toMatrix(m.joltage, N)
+
+	inputs := [][][]int{}
+	for _, input := range m.buttons {
+		fmt.Println(input)
+		im := toInputMatrix(input, N)
+		print2d(im)
+		inputs = append(inputs, im)
+	}
+
+	print2d(output)
+
+	return LinearProblem{
+		Inputs: inputs,
+		Output: output,
+		N:      N,
+	}
+}
+
 type Machine struct {
 	desiredState map[int]bool // desiredState as a map of [index]bool
 	buttons      [][]int
+	magicButtons []map[int][]int
 	joltage      []int
+}
+
+func (machine *Machine) initMagicButtons() {
+	for _, buttonGroup := range machine.buttons {
+		// find all the indexes this button matches and how high they go in the voltage..
+
+		multiplier := buttonGroup[0]
+		for _, button := range buttonGroup[1:] {
+			j := machine.joltage[button]
+			if j < multiplier {
+				multiplier = j
+			}
+		}
+		// now we create all multiples up to J?
+		m := map[int][]int{}
+		for i := 2; i < multiplier; i++ {
+			m[i] = func(input []int) []int {
+				out := make([]int, len(input))
+				for idx, num := range input {
+					out[idx] = num * i
+				}
+				return out
+			}(buttonGroup)
+		}
+
+		if len(m) != 0 {
+			machine.magicButtons = append(machine.magicButtons, m)
+		}
+	}
 }
 
 func copyMap(m map[int]bool) map[int]bool {
@@ -61,6 +175,12 @@ func (m Machine) solve2() int {
 	// start with all 0
 	start := make([]int, len(m.joltage))
 
+	// test magic buttons
+	m.initMagicButtons()
+	fmt.Println(m.joltage)
+	fmt.Println(m.magicButtons)
+	return 0
+
 	// 100 is a heuristic lol
 	minMoves := m.joltageSum()
 	maxMoves := m.joltageSum()
@@ -92,6 +212,7 @@ func (m Machine) solve2() int {
 			recurse(applyJoltage(buttons, copySlice(current)), moves+1)
 		}
 	}
+
 	recurse(start, 0)
 	return minMoves
 
@@ -151,6 +272,11 @@ func stateEquals(m1, m2 map[int]bool) bool {
 
 func main() {
 	machines := parse(readInput())
+	for _, m := range machines {
+		m.toLP().solver()
+		return
+	}
+	return
 	//fmt.Println(solve1(machines))
 	fmt.Println(solve2(machines))
 }
@@ -220,7 +346,7 @@ func parse(lines []string) []Machine {
 	return machines
 }
 func readInput() []string {
-	b, err := ioutil.ReadFile("input.txt")
+	b, err := ioutil.ReadFile("test_input.txt")
 	if err != nil {
 		panic(err)
 	}
